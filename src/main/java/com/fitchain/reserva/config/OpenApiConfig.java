ab758ef07560a5260b290d0773d8e345c0636843
neurott <neurott@protonmail.com>
2026-06-12 14:22:18 -0400

    faltaba la ip en el openapicfg

--- src/main/java/com/fitchain/reserva/config/OpenApiConfig.java
+++ src/main/java/com/fitchain/reserva/config/OpenApiConfig.java
@@ -18,6 +18,7 @@ public class OpenApiConfig {
                         .title("Reserva API - FitChain")
                         .description("API DE GESTIÓN DE LAS RESERVAS")
                         .version("1.0.0")).servers(List.of(
+                        new Server().url("http://44.197.145.9:8087").description("AWS"),
                         new Server().url("http://localhost:8087").description("Local")
                 ));
     }
